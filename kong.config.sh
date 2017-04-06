374de07368d8ac5ac8a02ccbaf421ff10f8c1799
Matheus Magalhaes <matheusr@cpqd.com.br>
2017-04-06 11:52:40 -0300

    [IOTMID-185] Initialization updates
    This commit adds the current version of the calls required to make
    kong work with the set of internal services.
    
    This also adds a new set of configuration to the yaml file, easing
    the issue where services (containers) would error and fail (exit)
    due to non-initialized dependencies. This is actually just a hot
    fix, since those services (notably, STH and Kong) should be able
    to recover from dependency errors on their own. On a more elaborate
    deploy scenario (i.e. using kubernetes or mesos instead of docker-compose)
    the "hypervisor" can be configured to reload failed containers on
    error.
    
    Change-Id: I49a4498a9b63dd6840e0ba92528a58dcdabb8d30

---
+++ kong.config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# ---  0.10.x
+kong="http://localhost:8001"
+
+(curl $kong/apis -sS -X PUT \
+    --header "Content-Type: application/json" \
+    -d @- | python -m json.tool) <<PAYLOAD
+{
+    "name": "gui",
+    "uris": "/",
+    "strip_uri": false,
+    "upstream_url": "http://gui:80"
+}
+PAYLOAD
+
+(curl $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- | python -m json.tool) <<PAYLOAD
+{
+    "name": "template",
+    "uris": "/template",
+    "strip_uri": false,
+    "upstream_url": "http://devm:5000"
+}
+PAYLOAD
+curl -X POST $kong/apis/template/plugins --data "name=jwt"
+
+(curl $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- | python -m json.tool) <<PAYLOAD
+{
+    "name": "device",
+    "uris": "/device",
+    "strip_uri": false,
+    "upstream_url": "http://devm:5000"
+}
+PAYLOAD
+curl -X POST $kong/apis/device/plugins --data "name=jwt"
+
+
+(curl $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- | python -m json.tool) <<PAYLOAD
+{
+    "name": "auth-service",
+    "uris": "/auth",
+    "strip_uri": true,
+    "upstream_url": "http://auth:5000"
+}
+PAYLOAD
+
+# (curl $kong/apis -s -S -X POST \
+#     --header "Content-Type: application/json" \
+#     -d @- | python -m json.tool) <<PAYLOAD
+# {
+#     "name": "flows",
+#     "uris": "/flows",
+#     "strip_uri": true,
+#     "upstream_url": "http://orch:5000"
+# }
+# PAYLOAD
