499afd82007fcf7423e697c0cb6c024217b90126
Giovanni Curiel dos Santos <gcuriel@cpqd.com.br>
2017-04-18 14:07:08 -0300

    [IOTMID-220] Updating test script for perseo
    This commit updates the test script for perseo, adding a
    few new and more interesting rules.
    
    Change-Id: Id343163150c596b3c5ce866d43e85e11cc28bc92

--- test/perseo.config.sh
+++ test/perseo.config.sh
@@ -1,121 +1,120 @@
-## Service configuration ##
-curl -X POST -H "Fiware-Service: devm" -H "Fiware-ServicePath: /" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
-        "services": [
-        {
-            "resource": "/devm",
-            "apikey": "nexus",
-            "type": "Nexus"
-        }
-        ]
-    }' 'http://iotagent:4041/iot/services'
+## Service creation ##
+curl -X POST -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+       "services": [
+       {
+           "resource": "/inspection_service",
+           "apikey": "container",
+           "type": "ContainerSensor"
+       }
+       ]
+   }' 'http://iotagent:4041/iot/services'
 
 
 ## Device creation ##
-curl -X POST -H "Fiware-Service: devm" -H "Fiware-ServicePath: /" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{ 
-    "devices": [ 
-        { 
-            "device_id": "mobile-1", 
-            "entity_name": "m1", 
-            "entity_type": "Nexus", 
-            "attributes": [ 
-                  { "object_id": "acceleration", "name": "Acceleration", "type": "string" }
-            ],
-            "internal_attributes":  {
-                "timeout" : {
-                    "sampleQueueMaxSize" : 4,
-                    "waitMultiplier" : 3 ,
-                    "minimumTimeoutBase" : 50 
-                }
-            }
-        }
-    ]
+curl -X POST -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+   "devices": [
+       {
+           "device_id": "maersk-lum-1",
+           "entity_name": "mk-lum-1",
+           "entity_type": "ContainerSensor",
+           "attributes": [
+                 { "object_id": "luminosity", "name": "Luminosity", "type": "string" }
+           ],
+           "internal_attributes":  {
+               "timeout" : {
+                   "sampleQueueMaxSize" : 4,
+                   "waitMultiplier" : 3 ,
+                   "minimumTimeoutBase" : 50
+               }
+           }
+       }
+   ]
 }' 'http://iotagent:4041/iot/devices'
 
 
-## Publishing new values ##
-mosquitto_pub -h mqtt -t /nexus/mobile-1/attrs -m '{"acceleration" : "10"}'
 
 
-## Just checking on orion if the attribute was set ##
-curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Fiware-Service: devm" -H "Fiware-ServicePath: /" -d '{
-    "entities": [
-        {
-            "isPattern": "false",
-            "id": "m1",
-            "type": "Nexus"
+## Perseo's subscription in orion' ##
+curl orion:1026/v1/subscribeContext -s -S -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection"  --header 'Content-Type: application/json' \
+   --header 'Accept: application/json' -d '
+{
+   "entities": [
+       {
+           "type": "ContainerSensor",
+           "isPattern": "false",
+           "id": "mk-lum-1"
+       }
+   ],
+   "attributes": [
+       "Luminosity"
+   ],
+   "reference": "http://perseo-fe:9090/notices",
+   "duration": "P1M",
+   "notifyConditions": [
+       {
+           "type": "ONCHANGE",
+           "condValues": [
+               "Luminosity"
+           ]
+       }
+   ]
+}'
+
+
+## Rules in perseo ##
+curl -X POST -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection" -H "Content-Type: application/json"  -d '{
+  "name":"luminosity_notif_01",
+  "text":"select *, \"luminosity_notif_01\" as ruleName, *, ev2.Luminosity? as LastLuminosity, ev.Luminosity? as Luminosity, ev.id? as Meter from pattern [every ev2=iotEvent((cast(cast(Luminosity?, String), float) > 9.0) and (type=\"ContainerSensor\")) -> ev=iotEvent((cast(cast(Luminosity?, String), float) < 5.0)  and (type=\"ContainerSensor\"))]",
+  "action":{
+     "type":"post",
+     "template":"The container was closed - ${Meter}, luminosity: ${Luminosity}, lastValue: ${LastLuminosity}",
+     "parameters":{
+        "url": "http://172.21.0.1:8081/dev/perseo-fe-01",
+        "method": "POST",
+        "headers": {
+           "Content-type": "text/plain"
         }
-    ]
+     }
+  }
+}'  http://perseo-fe:9090/rules
+
+
+curl -X POST -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection" -H "Content-Type: application/json"  -d '{
+  "name":"luminosity_notif_02",
+  "text":"select *, \"luminosity_notif_02\" as ruleName, *, ev2.Luminosity? as LastLuminosity, ev.Luminosity? as Luminosity, ev.id? as Meter from pattern [every ev2=iotEvent((cast(cast(Luminosity?, String), float) < 5.0) and (type=\"ContainerSensor\")) -> ev=iotEvent((cast(cast(Luminosity?, String), float) > 9.0) and (type=\"ContainerSensor\"))]",
+  "action":{
+     "type":"post",
+     "template":"The container was opened - ${Meter}, luminosity: ${Luminosity}, lastValue: ${LastLuminosity}",
+     "parameters":{
+        "url": "http://172.21.0.1:8081/dev/perseo-fe-02",
+        "method": "POST",
+        "headers": {
+           "Content-type": "text/plain"
+        }
+     }
+  }
+}'  http://perseo-fe:9090/rules
+
+## Removing rules from perseo ##
+# curl -X DELETE -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection"  http://perseo-fe:9090/rules/luminosity_notif_01
+# curl -X DELETE -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection"  http://perseo-fe:9090/rules/luminosity_notif_02
+# curl -X GET -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection"  http://perseo-fe:9090/rules/
+
+
+## Publishing new values ##
+mosquitto_pub -h mqtt -t /container/maersk-lum-1/attrs -m '{"luminosity" : "0"}'
+
+
+## Getting data from broker ##
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Fiware-Service: inspection_service" -H "Fiware-ServicePath: /container_inspection" -d '{
+   "entities": [
+       {
+           "isPattern": "false",
+           "id": "mk-lum-1",
+           "type": "ContainerSensor"
+       }
+   ]
 }' 'http://orion:1026/NGSI10/queryContext'
 
 
-## Orion subscription ##
-#curl orion:1026/v1/subscribeContext -s -S -H "Fiware-Service: devm" -H #"Fiware-ServicePath: /"  --header 'Content-Type: application/json' \
-#    --header 'Accept: application/json' -d '
-# {
-#    "entities": [
-#        {
-#            "type": "Nexus",
-#            "isPattern": "false",
-#            "id": "m1"
-#        }
-#    ],
-#    "attributes": [
-#        "Acceleration"
-#    ],
-#    "reference": "http://172.21.0.1:8080/dev/id",
-#    "duration": "P1M",
-#    "notifyConditions": [
-#        {
-#            "type": "ONCHANGE",
-#            "condValues": [
-#                "Acceleration"
-#            ]
-#        }
-#    ],
-#    "throttling": "PT5S"
-#}'
 
-## Perseo's subscription in orion ##
-curl orion:1026/v1/subscribeContext -s -S -H "Fiware-Service: devm" -H "Fiware-ServicePath: /"  --header 'Content-Type: application/json' \
-    --header 'Accept: application/json' -d '
-{
-    "entities": [
-        {
-            "type": "Nexus",
-            "isPattern": "false",
-            "id": "m1"
-        }
-    ],
-    "attributes": [
-        "Acceleration"
-    ],
-    "reference": "http://perseo-fe:9090/notices",
-    "duration": "P1M",
-    "notifyConditions": [
-        {
-            "type": "ONCHANGE",
-            "condValues": [
-                "Acceleration"
-            ]
-        }
-    ],
-    "throttling": "PT5S"
-}'
-
-
-## Registering a new filter in perseon ##
-curl -X POST http://perseo-fe:9090/rules -H "Fiware-Service: devm" -H "Fiware-ServicePath: /" -H "Content-Type: application/json"  -d '{
-   "name":"acceleration_notif_09",
-   "text":"select *, \"acceleration_notif_09\" as ruleName, *, ev.Acceleration? as Acceleration, ev.id? as Meter from pattern [every ev=iotEvent(cast(cast(Acceleration?, String), float) > 9 and type=\"Nexus\")]",
-   "action":{
-      "type":"post",
-      "template":"This is another notification - 09 - ${Acceleration}",
-      "parameters":{
-         "url": "http://172.21.0.1:8081/dev/perseo-fe-09",
-         "method": "POST",
-         "headers": {
-            "Content-type": "text/plain"
-         }
-      }
-   }
-}'
