0dfd9f12667b07e2a0ea1b4139c522e90625549a
Alexandre Vasconcellos <31312397+alexandre-vasc@users.noreply.github.com>
2017-12-29 15:51:49 -0200

    Add EJBCA service (#23)
    * Add EJBCA service
    Add mqtt-tls options
    
    * Unified ejbca-ca and ejbca-sign apis.

--- kong.config.sh
+++ kong.config.sh
@@ -159,3 +159,17 @@ PAYLOAD
 }
 PAYLOAD
 # no auth: used for middleware <-> device communication via HTTP(s)
+
+
+# CA certificate retrievemment and certificate sign requests
+(curl -o /dev/null $kong/apis -sS -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+     "name": "ejbca-paths",
+     "uris": [ "/sign", "/ca"],
+     "strip_uri": false,
+     "upstream_url": "http://ejbca:5583/"
+ }
+PAYLOAD
+authConfig "ejbca-paths"
