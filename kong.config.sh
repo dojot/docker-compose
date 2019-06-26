505cd6a0ee5221425324ee48097dfce590140e47
Mariane Previde <mprevide@cpqd.com.br>
2019-06-26 13:06:50 -0300

    add iot agent mosca metrics service

--- kong.config.sh
+++ kong.config.sh
@@ -254,3 +254,16 @@ PAYLOAD
 }
 PAYLOAD
 authConfig "cron"
+
+# iotagent-mqtt
+(curl -o /dev/null ${kong}/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "iotagent-mqtt",
+    "uris": ["/iotagent-mqtt/metrics"],
+    "strip_uri": false,
+    "upstream_url": "http://iotagent-mqtt:10001/"
+}
+PAYLOAD
+authConfig "iotagent-mqtt"
