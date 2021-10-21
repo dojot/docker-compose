66b0509b2ba1a4fc0822216e860c3ce82b6a4cb2
Daniela Petito <daniela@cpqd.com.br>
2021-10-21 15:57:42 -0300

    docker-compose: Updates

--- secrets/create_secrets.sh
+++ secrets/create_secrets.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 ##############################
-# Secrets kafk-ws
-##############################
-if [ ! -s secrets/KAFKA_WS_TICKET_SECRET ]; then
-  openssl passwd -5 `uname -n ``date +%F%H%M%S`KAFKA_WS_TICKET_SECRET > secrets/KAFKA_WS_TICKET_SECRET
-fi
-
-##############################
 # Secrets minio
 ##############################
 if [ ! -s secrets/MINIO_SECRET_KEY ]; then
@@ -18,13 +11,7 @@ if [ ! -s secrets/MINIO_ACCESS_KEY ]; then
   openssl passwd -5 `uname -n ``date +%F%H%M%S`MINIO_ACCESS_KEY > secrets/MINIO_ACCESS_KEY
 fi
 
-##############################
-# Secrets image-manager
-##############################
-if [ ! -s secrets/S3SECRETKEY ]; then
-  openssl passwd -5 `uname -n ``date +%F%H%M%S`S3SECRETKEY > secrets/S3SECRETKEY
-fi
-
-if [ ! -s secrets/S3ACCESSKEY ]; then
-  openssl passwd -5 `uname -n ``date +%F%H%M%S`S3ACCESSKEY > secrets/S3ACCESSKEY
+# Valida criação secret
+if [ ! -s secrets/MINIO_SECRET_KEY ] || [ ! -s secrets/MINIO_ACCESS_KEY ]; then
+  exit 1
 fi
