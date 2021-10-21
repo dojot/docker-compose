c8b4e51c1162e09bbdf9afe8ae82ca38efa1c87f
Daniela Petito <83663340+danielapetito@users.noreply.github.com>
2021-10-21 16:09:49 -0300

    Update create_secrets.sh

--- secrets/create_secrets.sh
+++ secrets/create_secrets.sh
@@ -4,11 +4,11 @@
 # Secrets minio
 ##############################
 if [ ! -s secrets/MINIO_SECRET_KEY ]; then
-  openssl passwd -5 `uname -n ``date +%F%H%M%S`MINIO_SECRET_KEY > secrets/MINIO_SECRET_KEY
+  $(openssl passwd -5 `uname -n ``date +%F%H%M%S`MINIO_SECRET_KEY > secrets/MINIO_SECRET_KEY)
 fi
 
 if [ ! -s secrets/MINIO_ACCESS_KEY ]; then
-  openssl passwd -5 `uname -n ``date +%F%H%M%S`MINIO_ACCESS_KEY > secrets/MINIO_ACCESS_KEY
+  $(openssl passwd -5 `uname -n ``date +%F%H%M%S`MINIO_ACCESS_KEY > secrets/MINIO_ACCESS_KEY)
 fi
 
 # Valida criação secret
