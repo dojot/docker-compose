e27e1d93d158feb767b01f5a0a13891d2c71f74e
Daniela Petito <83663340+danielapetito@users.noreply.github.com>
2021-10-25 09:57:06 -0300

    docker-compose: All secrets must always be different (#271)
    * docker-compose:  All secrets must always be different
    
    * docker-compose: Updates
    
    * Update create_secrets.sh
    
    * Update create_secrets.sh
    
    * update permission files

---
+++ secrets/create_secrets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+##############################
+# Secrets minio
+##############################
+if [ ! -s secrets/MINIO_SECRET_KEY ]; then
+  openssl passwd -5 $(uname -n)$(date +%F%H%M%S)MINIO_SECRET_KEY > secrets/MINIO_SECRET_KEY
+fi
+
+if [ ! -s secrets/MINIO_ACCESS_KEY ]; then
+  openssl passwd -5 $(uname -n)$(date +%F%H%M%S)MINIO_ACCESS_KEY > secrets/MINIO_ACCESS_KEY
+fi
+
+# Valida criação secret
+if [ ! -s secrets/MINIO_SECRET_KEY ] || [ ! -s secrets/MINIO_ACCESS_KEY ]; then
+  exit 1
+fi
