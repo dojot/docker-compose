402c0978cc1e1016eb3a9b08579776e23c2c4289
Heitor Silva <heitor.silva@meta.com.br>
2021-12-13 13:48:02 -0300

    dojot/dojot#2382 - Geração da secret no cofre

--- secrets/create_secrets.sh
+++ secrets/create_secrets.sh
@@ -15,3 +15,15 @@ fi
 if [ ! -s secrets/MINIO_SECRET_KEY ] || [ ! -s secrets/MINIO_ACCESS_KEY ]; then
   exit 1
 fi
+
+##############################
+# Secrets keycloak proxy
+##############################
+if [ ! -s secrets/.KEYCLOAK_PROXY ]; then
+  openssl passwd -5 $(uname -n)$(date +%F%H%M%S).KEYCLOAK_PROXY > secrets/.KEYCLOAK_PROXY
+fi
+
+# Valida criação secret
+if [ ! -s secrets/.KEYCLOAK_PROXY ] || [ ! -s secrets/.KEYCLOAK_PROXY ]; then
+  exit 1
+fi
