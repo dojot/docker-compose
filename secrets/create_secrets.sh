3de2f12f53dffd0684e2f3ded9c771c6d2c695e9
Rodrigo A. S. Maia <ramaia@cpqd.com.br>
2022-02-01 15:54:49 -0300

    Update: automatic creation of secrets for services

--- secrets/create_secrets.sh
+++ secrets/create_secrets.sh
@@ -27,6 +27,22 @@ if [ ! -s secrets/KEYCLOAK_PROXY_USER ]; then
   openssl passwd -5 $(uname -n)$(date +%F%H%M%S)KEYCLOAK_PROXY_USER > secrets/KEYCLOAK_PROXY_USER
 fi
 
+###########################################################
+# Secrets clients keycloak 
+#
+# The create secrets using to json do Realm Representation 
+###########################################################
+
+# Install JQ
+apt-get install jq -y
+
+for NAME in $(jq ".clients[].clientId" /opt/dojot/customRealmRepresentation.json |  grep 'dojot' | sed "s/\"/ /g" )
+do
+    if [ ! -s secrets/${NAME} ]; then
+        openssl passwd -5 $(uname -n)$(date +%F%H%M%S)${NAME} > secrets/${NAME}
+    fi
+done 
+
 # Valida criação secret
 if [ ! -s secrets/KEYCLOAK_PROXY ] || [ ! -s secrets/KEYCLOAK_PROXY_USER ]; then
   exit 1
