0a30031f419d32305b365c6a55f02d2f813a3ef5
Luan Eduardo da Costa <luan1346@gmail.com>
2022-10-26 16:35:32 -0300

    Add report manager service on kong config

--- kong/kong.config.sh
+++ kong/kong.config.sh
@@ -148,6 +148,8 @@ addAuthToEndpoint "file-mgmt"
 
 createEndpoint "minio-files" "http://minio-files:9000"  '"/minio-files"' "true"
 
+createEndpoint "report-manager" "http://report-manager:3791"  '"/report-manager"' "true"
+
 createEndpoint "container-nx" "http://container-nx:80" '"/v2"' "true"
 createEndpoint "common-nx" "http://common-nx:80" '"/mfe/common"' "true"
 createEndpoint "home-nx" "http://home-nx:80" '"/mfe/home"' "true"
